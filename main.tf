c4437fc057d2efaec2d42a9988b7a2b6f4f43fcf
laba <labanoelie@gmail.com>
2023-04-04 18:11:15 -0400

    create EKS

--- main.tf
+++ main.tf
@@ -12,16 +12,54 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-2384103847123487"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "eks_subnet" {
+  vpc_id = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_security_group" "eks_security_group" {
+  name_prefix = "eks"
+  vpc_id = aws_vpc.eks_vpc.id
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+resource "aws_eks_cluster" "eks_cluster" {
+  name = "eks-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+  vpc_config {
+    subnet_ids = [aws_subnet.eks_subnet.id]
+    security_group_ids = [aws_security_group.eks_security_group.id]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.eks_cluster.name
 }
