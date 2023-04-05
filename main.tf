6550ffdd474bb09dbbf7278d35fb6115d313c1cb
laba <labanoelie@gmail.com>
2023-04-04 20:07:07 -0400

    create EKS

--- main.tf
+++ main.tf
@@ -13,104 +13,222 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+
+locals {
+  cluster_name = "my-eks-cluster" # Replace with your desired cluster name
 }
 
-resource "aws_subnet" "subnet_1a" {
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-resource "aws_subnet" "subnet_1b" {
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-resource "aws_eks_cluster" "my_cluster" {
-  name = "my-eks-cluster"
+resource "aws_eks_cluster" "this" {
+  name     = local.cluster_name
   role_arn = aws_iam_role_policy_attachment.eks_cluster.arn
 
   depends_on = [
-    aws_subnet.subnet_1a.id,
-    aws_subnet.subnet_1b.id
+    aws_iam_role_policy_attachment.eks_cluster
+  ]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster
   ]
 
   vpc_config {
-    subnet_ids = [
-      aws_subnet.subnet_1a.id,
-      aws_subnet.subnet_1b.id
-    ]
+    subnet_ids = aws_subnet.private.*.id
   }
-}
-data "aws_eks_cluster" "my_cluster" {
-  name = aws_eks_cluster.my_cluster.name
+
+  depends_on = [
+    aws_vpc.this,
+    aws_subnet.private
+  ]
+
+  depends_on = [
+    aws_eks_cluster_auth.this
+  ]
 }
 
-resource "aws_iam_policy" "eks_cluster" {
-  name = "eks-cluster-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "eks:DescribeCluster",
-          "eks:ListClusters",
-          "eks:TagResource",
-          "eks:UntagResource",
-        ]
-        Effect = "Allow"
-        Resource = data.aws_eks_cluster.my_cluster.arn
-      },
-      {
-        Action = [
-          "eks:CreateFargateProfile",
-          "eks:DeleteFargateProfile",
-          "eks:DescribeFargateProfile",
-          "eks:ListFargateProfiles",
-          "eks:UpdateFargateProfile",
-        ]
-        Effect = "Allow"
-        Resource = aws_eks_cluster.my_cluster.arn
-      },
-      {
-        Action = [
-          "ec2:DescribeSubnets",
-          "ec2:DescribeRouteTables",
-        ]
-        Effect = "Allow"
-        Resource = aws_subnet.subnet_1a.arn
-      },
-      {
-        Action = [
-          "ec2:DescribeSubnets",
-          "ec2:DescribeRouteTables",
-        ]
-        Effect = "Allow"
-        Resource = aws_subnet.subnet_1b.arn
-      },
-    ]
-  })
-}
 resource "aws_iam_role_policy_attachment" "eks_cluster" {
   policy_arn = aws_iam_policy.eks_cluster.arn
   roles      = [aws_iam_role.eks_cluster.name]
 }
 
-resource "aws_iam_role" "eks_cluster" {
-  name = "eks-cluster"
-  assume_role_policy = jsonencode({
+resource "aws_iam_role_policy_attachment" "eks_cluster_ro" {
+  policy_arn = aws_iam_policy.eks_cluster_ro.arn
+  roles      = [aws_iam_role.eks_cluster.name]
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_vpc" {
+  policy_arn = aws_iam_policy.eks_cluster_vpc.arn
+  roles      = [aws_iam_role.eks_cluster_vpc.name]
+}
+
+resource "aws_iam_policy_attachment" "eks_cluster_ingress_policy_attachment" {
+  policy_arn = aws_iam_policy.eks_cluster_ingress.arn
+  roles      = [aws_iam_role.eks_cluster.name]
+}
+
+resource "aws_iam_policy" "eks_cluster" {
+  name_prefix = local.cluster_name
+
+  policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "eks.amazonaws.com"
+
+    Statement = [
+      {
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListClusters",
+          "eks:AccessKubernetesApi"
+        ]
+
+        Effect = "Allow"
+        Resource = "*"
       }
-    }]
+    ]
   })
+}
+
+resource "aws_iam_policy" "eks_cluster_ro" {
+  name_prefix = local.cluster_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListClusters"
+        ]
+
+        Effect = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "eks_cluster_vpc" {
+  name_prefix = local.cluster_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Action = [
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets"
+        ]
+
+        Effect = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "eks_cluster_ingress" {
+  name_prefix = local.cluster_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Action = [
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeSecurityGroups",
+          "ec2:CreateTags",
+          "ec2:DescribeTags"
+        ]
+
+        Effect = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16" # Replace with your desired VPC CIDR block
+
+  tags = {
+    Name = local.cluster_name
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = 2 # Create 2 subnets, one in each AZ
+
+  cidr_block = "10.0.${count.index + 1}.0/24" # Replace with your desired subnet CIDR blocks
+  vpc_id     = aws_vpc.this.id
+
+  tags = {
+    Name = "${local.cluster_name}-private-${count.index + 1}"
+  }
+}
+
+resource "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+
+  depends_on = [
+    aws_eks_cluster.this
+  ]
+}
+
+data "aws_availability_zones" "this" {
+  state = "available"
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = local.cluster_name
+  role_arn = aws_iam_role_policy_attachment.eks_cluster.arn
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster
+  ]
+
+  vpc_config {
+    subnet_ids = aws_subnet.private.*.id
+  }
+
+  depends_on = [
+    aws_vpc.this,
+    aws_subnet.private
+  ]
+
+  depends_on = [
+    aws_eks_cluster_auth.this
+  ]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_ro,
+    aws_iam_role_policy_attachment.eks_cluster_vpc,
+    aws_iam_policy_attachment.eks_cluster_ingress_policy_attachment
+  ]
+
+  depends_on = [
+    aws_iam_policy.eks_cluster,
+    aws_iam_policy.eks_cluster_ro,
+    aws_iam_policy.eks_cluster_vpc,
+    aws_iam_policy.eks_cluster_ingress
+  ]
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_arn" {
+  value = aws_eks_cluster.this.arn
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.this.certificate_authority.0.data
 }
