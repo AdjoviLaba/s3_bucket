297711634e82d8b73936d6fbc53903c271cb5bf4
laba <labanoelie@gmail.com>
2023-04-07 14:51:44 -0400

    update

--- main.tf
+++ main.tf
@@ -1,15 +1,26 @@
 provider "aws" {
   region = "us-east-1" 
   }
-module "eks_cluster" {
-  source = "terraform-aws-modules/eks/aws"
-  
-  cluster_name = "my-eks-cluster2"
-  
-  vpc_subnets = ["subnet-0d529dd75a00ba93c", "subnet-06ae8c8f74c4269ce"] # replace with your desired subnets
-  
-  tags = {
-    Terraform   = "true"
-    Environment = "qa"
+resource "aws_eks_cluster" "example" {
+  name     = "example"
+  role_arn = aws_iam_role.example.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
   }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
 }
