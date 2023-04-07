7674907e523ac2509e50cc5fba8ecb7f2ebe5422
laba <labanoelie@gmail.com>
2023-04-07 16:43:31 -0400

    update

--- main.tf
+++ main.tf
@@ -1,12 +1,55 @@
-provider "aws" {
-  region = "us-east-1" 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
-resource "aws_eks_cluster" "example" {
-  name     = "example"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    tags =  {
+        Name = "main"
+    }
+}
+# Declare the data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# e.g., Create subnets in the first two available availability zones
+
+resource "aws_subnet" "primary" {
+  vpc_id     = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[0]
+   cidr_block = "10.0.1.0/24" # Specify the CIDR block for the primary subnet
+
+
+  # ...
+}
+
+resource "aws_subnet" "secondary" {
+  vpc_id     = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[1]
+   cidr_block = "10.0.2.0/24" # Specify the CIDR block for the primary subnet
+
+
+  # ...
+}
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "eks_cluster"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+    subnet_ids = [aws_subnet.primary.id, aws_subnet.secondary.id]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
@@ -16,3 +59,27 @@ resource "aws_eks_cluster" "example" {
     aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
   ]
 }
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "example1" {
+  name               = "eks-cluster-example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.example.name
+}
+
