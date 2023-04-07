e1e7bd278637741c7ba5a3a0c0f4b0845434ba8f
laba <labanoelie@gmail.com>
2023-04-07 16:47:16 -0400

    update

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ terraform {
     }
   }
 }
+locals {
+  workspace_name = "${terraform.workspace}"
+}
 
 # Configure the AWS Provider
 provider "aws" {
@@ -83,3 +86,9 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
   role       = aws_iam_role.example.name
 }
 
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.example.name
+}
